8b30fe097c425dd662493289fdb5362cf099442c
whitegigs <110684302+whitegigs@users.noreply.github.com>
2023-05-14 01:33:20 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,37 @@
 import requests
 import pandas as pd
-# from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup as bs
 import streamlit as st
+from datetime import datetime
+
+
+
+now = datetime.now()
+today = now.strftime("%Y-%m-%d %H:%M:%S")
+st.text('오늘 날짜 '+ today)
+st.title('오늘의 최신 부산 뉴스')
 
 
 
 with open('css.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
+keyword = st.text_input('키워드를 입력하시면 [부산 + 키워드] 와 관련된 최신 뉴스 링크를 보여드립니다. 예) 아시아드 주 경기장, 북구 가볼만한 곳, 축제 등'," ")
+st.write('부산관광 ', keyword)
+
 i = 0
-url = f'https://search.naver.com/search.naver?where=news&sm=tab_jum&query=부산관광&start={i}' 
-
-
-# for i in range(1,30,10):
-#     re = requests.get(url)
-#     html = re.text
-#     soup = bs(html,'html.parser')
-
-#     news = soup.select('.news_tit')
-
-#     for i in news:
-#         title = i.text
-#         link = i.attrs['href']
-#         st.markdown(f'<ul><li><font size=2><a href="{link}">{title}</a>', unsafe_allow_html=True)
 
 
 
+for i in range(1,30,10):
+    url = f'https://search.naver.com/search.naver?where=news&sm=tab_jum&query=부산관광+{keyword}&start={i}' 
+    re = requests.get(url)
+    html = re.text
+    soup = bs(html,'html.parser')
 
+    news = soup.select('.news_tit')
+
+    for i in news:
+        title = i.text
+        link = i.attrs['href']
+        st.markdown(f'<ul><li><font size=2><a href="{link}">{title}</a>', unsafe_allow_html=True)

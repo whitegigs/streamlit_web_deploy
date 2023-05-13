cfd00ae33ec9aca2067b619ab271812b25dd60c8
whitegigs <110684302+whitegigs@users.noreply.github.com>
2023-05-14 01:36:27 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,8 @@ st.title('오늘의 최신 부산 뉴스')
 
 
 
-with open('css.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+# with open('css.css') as f:
+#     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 keyword = st.text_input('키워드를 입력하시면 [부산 + 키워드] 와 관련된 최신 뉴스 링크를 보여드립니다. 예) 아시아드 주 경기장, 북구 가볼만한 곳, 축제 등'," ")
 st.write('부산관광 ', keyword)

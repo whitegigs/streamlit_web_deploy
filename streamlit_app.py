68c07270cbd9f212ccb508cf2a7856775eb79eec
whitegigs <110684302+whitegigs@users.noreply.github.com>
2023-05-14 01:44:07 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.title('오늘의 최신 부산 뉴스')
 keyword = st.text_input('키워드를 입력하시면 [부산 + 키워드] 와 관련된 최신 뉴스 링크를 보여드립니다. 예) 아시아드 주 경기장, 북구 가볼만한 곳, 축제 등'," ")
 st.write('부산관광 ', keyword)
 
-# i = 0
+i = 0
 
 
 
@@ -20,11 +20,11 @@ for i in range(1,30,10):
     url = f'https://search.naver.com/search.naver?where=news&sm=tab_jum&query=부산관광+{keyword}&start={i}' 
     re = requests.get(url)
     html = re.text
-    soup = bs(html,'html.parser')
+   
+st.text(html)
+#     news = soup.select('.news_tit')
 
-    news = soup.select('.news_tit')
-
-    for i in news:
-        title = i.text
-        link = i.attrs['href']
-        st.markdown(f'<ul><li><font size=2><a href="{link}">{title}</a>', unsafe_allow_html=True)
+#     for i in news:
+#         title = i.text
+#         link = i.attrs['href']
+#         st.markdown(f'<ul><li><font size=2><a href="{link}">{title}</a>', unsafe_allow_html=True)

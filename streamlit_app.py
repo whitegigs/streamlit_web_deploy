1937506ace332ca68c7e7d656649b55d3a70ff32
whitegigs <110684302+whitegigs@users.noreply.github.com>
2023-05-14 01:51:43 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,20 @@
-import requests
-import pandas as pd
 import streamlit as st
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup as bs
+import time
+import random
+from datetime import date
+import pandas as pd
 
 
 
+st.set_page_config(layout="wide")
 
-st.title('오늘의 최신 부산 뉴스')
-
-
-
-keyword = st.text_input('키워드를 입력하시면 [부산 + 키워드] 와 관련된 최신 뉴스 링크를 보여드립니다. 예) 아시아드 주 경기장, 북구 가볼만한 곳, 축제 등'," ")
-st.write('부산관광 ', keyword)
+base="light"
+secondaryBackgroundColor="#f1f1f1"
+font="monospace"
 
 
-
-
-for i in range(1,11):
-    url = f'https://search.naver.com/search.naver?where=news&sm=tab_jum&query=부산관광+{keyword}&start={i}' 
-    re = requests.get(url)
-#     html = re.text
-   
-    st.text(re)
-#     news = soup.select('.news_tit')
-
-#     for i in news:
-#         title = i.text
-#         link = i.attrs['href']
-#         st.markdown(f'<ul><li><font size=2><a href="{link}">{title}</a>', unsafe_allow_html=True)
+st.title('오늘의 쿠팡')

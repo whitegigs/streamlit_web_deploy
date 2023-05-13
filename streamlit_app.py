e61610b9570c1a3c6c86eea6b2667c913d2d0098
whitegigs <110684302+whitegigs@users.noreply.github.com>
2023-05-14 01:31:56 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import requests
 import pandas as pd
-from bs4 import BeautifulSoup as bs
+# from bs4 import BeautifulSoup as bs
 import streamlit as st
 
 
@@ -12,17 +12,17 @@ i = 0
 url = f'https://search.naver.com/search.naver?where=news&sm=tab_jum&query=부산관광&start={i}' 
 
 
-for i in range(1,30,10):
-    re = requests.get(url)
-    html = re.text
-    soup = bs(html,'html.parser')
+# for i in range(1,30,10):
+#     re = requests.get(url)
+#     html = re.text
+#     soup = bs(html,'html.parser')
 
-    news = soup.select('.news_tit')
+#     news = soup.select('.news_tit')
 
-    for i in news:
-        title = i.text
-        link = i.attrs['href']
-        st.markdown(f'<ul><li><font size=2><a href="{link}">{title}</a>', unsafe_allow_html=True)
+#     for i in news:
+#         title = i.text
+#         link = i.attrs['href']
+#         st.markdown(f'<ul><li><font size=2><a href="{link}">{title}</a>', unsafe_allow_html=True)
 
 
 
